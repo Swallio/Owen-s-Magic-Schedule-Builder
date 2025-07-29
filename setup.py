08e4283ee5a862b8af3ec502beb6aa9022a914fa
Swallio <32783778+Swallio@users.noreply.github.com>
2025-07-29 10:42:20 -0700

    Remove icon file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+APP = ['Magic Schedule Builder.py']
+DATA_FILES = ['icon.icns']
+OPTIONS = {
+    'argv_emulation': True,
+    'iconfile': 'icon.icns',
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
